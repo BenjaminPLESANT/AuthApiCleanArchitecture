1b3867816768c9c2b507d6fece439ebd423d956d
Benjamin Plesant <104313133+BenjaminPLESANT@users.noreply.github.com>
2024-09-16 16:44:39 +0200

    Update LoginRequestDto.cs

--- AuthApi.Application/DTOs/LoginRequestDto.cs
+++ AuthApi.Application/DTOs/LoginRequestDto.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 ﻿namespace AuthApi.Application.DTOs;
 
 public class LoginRequestDto
 {
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email address format.")]
     public string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    [DataType(DataType.Password)]
     public string Password { get; set; }
 }
